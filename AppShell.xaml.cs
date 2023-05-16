db80966ae9699ae6629c1ba2f63dea3cb261edae
Moussa <salisoumoussa1@gmail.com>
2023-05-16 20:38:39 +0200

    Aggiungere i file di progetto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiMediaElement;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiMediaElement;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		if (mediaElement.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
+            mediaElement.Pause();
+		else if(mediaElement.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused)
+            mediaElement.Play();
+	}
+}
+
